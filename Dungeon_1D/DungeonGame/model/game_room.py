51450d27b7f8d67ed7a35c921b02012c63c85885
SteveK <stevendeankennedy@gmail.com>
2016-11-08 11:58:00 -0500

    Made a bunch of changes to allow for moving entities.

--- Dungeon_1D/DungeonGame/model/game_room.py
+++ Dungeon_1D/DungeonGame/model/game_room.py
@@ -21,6 +21,14 @@ class GameRoom:
         else:
             return False
 
+    def add_entity_at_pos(self, entity, p):
+        if p < 0 or p >= len(self.spaces):
+            return False
+        if self.spaces[p] is None:
+            self.spaces[p] = entity
+            return True
+        return False
+
     def add_obstacle(self, entity):
         """
         Puts something at the back of the room.
@@ -34,7 +42,37 @@ class GameRoom:
         else:
             return False
 
-    def update(self, dt):
+    def finish_update(self):
+        """
+        Confirm that room has been updated.
+        Makes sure all objects will be updated next time around.
+        This is necessary since entities can change rooms.
+        """
         for e in self.spaces:
             if e is not None:
+                e.update_ready()
+
+    def update(self, dt):
+
+        updated = {}  # objects that should be adjusted here
+
+        # Update all objects
+        # for e in self.spaces:
+        for i in range(len(self.spaces)):
+            e = self.spaces[i]
+            if e is not None:
                 e.update(dt)
+                if e.move > 0:
+                    self.spaces[i] = None  # remove from room temporarily
+                    updated[e] = i  # e as key for attempted update along with original position
+
+        for e, pos in updated.items():
+            mov = e.move
+            npos = pos + mov
+
+            if (npos >= len(self.spaces)) and (self.next is not None):
+                self.next.add_entity(e)
+            else:
+                self.add_entity_at_pos(e, npos)
+
+            e.updated()
--- Dungeon_1D/DungeonGame/model/game_space.py
+++ Dungeon_1D/DungeonGame/model/game_space.py
@@ -12,12 +12,19 @@ class GameSpace(object):
         self.rooms = rooms
         # Set up exit pointers
         for i in range(len(self.rooms) - 1):  # last room's pointer stays None
-            self.rooms[i].next = i + 1
+            self.rooms[i].next = self.rooms[i + 1]
 
     def add(self, entity, room):
         success = self.rooms[room].add_entity(entity)
         return success
 
     def update(self, dt):
+        # before updates
+
+        # updates
         for room in self.rooms:
             room.update(dt)
+
+        # after updates
+        for room in self.rooms:
+            room.finish_update()
